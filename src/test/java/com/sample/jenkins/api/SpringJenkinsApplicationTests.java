640db9b145ca6375810e6c32761158f37cffe582
danielgarcia <danielgarcia@dgarcia.com>
2020-09-18 23:24:23 +0200

    Error intencionado

--- src/test/java/com/sample/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/sample/jenkins/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	
 	@Test
-	void contextLoads() {
+	public contextLoads() {
 		logger.info("Ejecución de los test...");
 		logger.info("Ejecución de los test segundo...");
 		assertEquals(true, true);
